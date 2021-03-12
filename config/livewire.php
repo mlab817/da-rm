eeaa540914e718a4a4cac719966d681eae3ce418
mlab817 <mlab817@gmail.com>
2021-03-12 11:25:34 +0800

    updated validation for files

--- config/livewire.php
+++ config/livewire.php
@@ -82,11 +82,11 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => 'google',        // Example: 'local', 's3'              Default: 'default'
+        'disk' => null,        // Example: 'local', 's3'              Default: 'default'
         'rules' => [
             'max:12288'
         ],       // Example: ['file', 'mimes:png,jpg']  Default: ['required', 'file', 'max:12288'] (12MB)
-        'directory' => '1QGJS3a_lxOgl4zncm35uwpPwmX8vrTja',   // Example: 'tmp'                      Default  'livewire-tmp'
+        'directory' => null,   // Example: 'tmp'                      Default  'livewire-tmp'
         'middleware' => 'throttle:5,1',  // Example: 'throttle:5,1'             Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs.
             'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
